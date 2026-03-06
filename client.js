fe402d1bcb6bbc3b87c087ee23e4aead7f003a03
goldbergaviv-png <goldberg.aviv@gmail.com>
2026-03-06 13:41:19 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -7,22 +7,34 @@ window.TrelloPowerUp.initialize({
         return t.popup({
           title: 'Print Card Barcode',
           url: './popup.html?mode=card',
-          height: 260
+          height: 650
         });
       }
     }];
   },
 
   'list-actions': function (t) {
-    return [{
-      text: 'Print List Barcode',
-      callback: function (t) {
-        return t.popup({
-          title: 'Print List Barcode',
-          url: './popup.html?mode=list',
-          height: 260
-        });
+    return [
+      {
+        text: 'Print List Barcode',
+        callback: function (t) {
+          return t.popup({
+            title: 'Print List Barcode',
+            url: './popup.html?mode=list',
+            height: 650
+          });
+        }
+      },
+      {
+        text: 'Print ALL Card Barcodes',
+        callback: function (t) {
+          return t.popup({
+            title: 'Print All Card Barcodes',
+            url: './popup.html?mode=allcards',
+            height: 650
+          });
+        }
       }
-    }];
+    ];
   }
 });
