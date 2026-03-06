2bb9f7338fda13d9ef811f08f0cb36f55259e634
goldbergaviv-png <goldberg.aviv@gmail.com>
2026-03-06 14:51:50 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -6,15 +6,8 @@ Overwrite these files in your GitHub repo root:
 - logo.jpeg
 - icon.png
 
-Fixes in v7:
-- Print button added back
-- Close button added
-- Logo constrained to fit 60mm label width
-- Second description line fixed
-- Cache-busting added (?v=7)
-
-After upload:
-1. Commit changes
-2. Wait 30-60 seconds
-3. Hard refresh Trello
-4. If needed, disable and re-enable the Power-Up
+Fix in v8:
+- List barcode printing now passes list ID + name directly from the clicked list
+- This fixes the bug where only the first viewed list printed and all later lists were blank
+- Print and Close buttons remain
+- Cache-busting updated to ?v=8
--- client.js
+++ client.js
@@ -7,7 +7,7 @@ window.TrelloPowerUp.initialize({
       callback: function (t) {
         return t.popup({
           title: 'Print Card Barcode',
-          url: './popup.html?v=7&mode=card',
+          url: './popup.html?v=8&mode=card',
           height: 700
         });
       }
@@ -19,10 +19,18 @@ window.TrelloPowerUp.initialize({
       {
         text: 'Print List Barcode',
         callback: function (t) {
-          return t.popup({
-            title: 'Print List Barcode',
-            url: './popup.html?v=7&mode=list',
-            height: 700
+          return t.list('id', 'name').then(function(list) {
+            const url =
+              './popup.html?v=8&mode=list' +
+              '&listId=' + encodeURIComponent(list.id) +
+              '&listName=' + encodeURIComponent(list.name || '') +
+              '&ts=' + Date.now();
+
+            return t.popup({
+              title: 'Print List Barcode',
+              url: url,
+              height: 700
+            });
           });
         }
       },
@@ -31,7 +39,7 @@ window.TrelloPowerUp.initialize({
         callback: function (t) {
           return t.popup({
             title: 'Print All Card Barcodes',
-            url: './popup.html?v=7&mode=allcards',
+            url: './popup.html?v=8&mode=allcards&ts=' + Date.now(),
             height: 700
           });
         }
