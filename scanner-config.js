a31b6b7f66eb94788d4778f72f6a7d1693d877cb
goldbergaviv-png <goldberg.aviv@gmail.com>
2026-03-08 16:17:32 +0200

    Add files via upload

--- scanner-config.js
+++ scanner-config.js
@@ -1,4 +1,4 @@
 window.SCANNER_CONFIG = {
-  apiKey: "PASTE_YOUR_TRELLO_API_KEY_HERE",
-  token: "PASTE_YOUR_TRELLO_TOKEN_HERE"
+  apiKey: "f578b488e456b77aaad372b0f846a0eb",
+  token: "ATTAe79ac0f0efd906cc6b080ac807b276fef15af80fcd130ac3ab83b4baee264874C6DC08C6"
 };
